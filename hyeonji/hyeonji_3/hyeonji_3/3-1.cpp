4b07efebb6bd3ac24e8a6362790e15e250d6dbb5
PRAHE <wendydeer1020@gmail.com>
2024-02-10 01:44:14 +0900

    Feat: "Correct 3-1 how to input"

--- hyeonji/hyeonji_3/hyeonji_3/3-1.cpp
+++ hyeonji/hyeonji_3/hyeonji_3/3-1.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 using namespace std;
 
@@ -10,37 +11,67 @@ private:
 	char* job;
 
 public:
-	Employee(char* name, int age, char* country, char* job) {
-		name = new char[10];
-		strcpy(this->name, name);
-		this->age = age;
-		country = new char[10];
-		strcpy(this->country, country);
-		job = new char[10];
-		strcpy(this->job, job);
-	}
+	Employee(char* name, int age, char* country, char* job);
 	bool isNameCorrect(char* name);
 	void print();
-	void exit();
-	void insert(char* name, int age, char* country, char* job);
-	void find(char* name);
-	void change(char* name, int age, char* country, char* job);
+	void change(char* name, int age, char* countrycountry, char* job);
+
+	void setName(char* name) { this->name = name; }
+	string getName() { return this->name; }
+
+	void setAge(int age) { this->age = age; }
+	int getAge() { return this->age; }
+
+	void setCountry(char* country) { this->country = country; }
+	string getCountry() { return this->country; }
+
+	void setJob(char* job) { this->job = job; }
+	string getJob() { return this->job; }
 };
 
+Employee::Employee(char* name, int age, char* country, char* job){
+	setName(name);
+	setAge(age); //save age
+	setCountry(country);
+	setJob(job);
+}
+
 int main() {
-	char str[20];
-	char* temp = strtok(str, " ");
+	char str[50];
+	char command[10];
+	char name[10];
+	int age;
+	char country[10];
+	char job[10];
+
 	cin >> str;
 
-	while (temp != NULL) { //chop the str
-		
-		temp = strtok(NULL, " ");
+	while (1) {
+		if (strcmp(str, "exit") == 0) {
+			break;
+		}
+		else if (strcmp(str, "insert") == 0) {
+			cin >> name;
+			cin >> age;
+			cin >> country;
+			cin >> job;
+			Employee* employee = new Employee(name, age, country, job);
+			break;
+		}
+		else if (strcmp(str, "find") == 0) {
+
+		}
+		else if (strcmp(str, "change") == 0) {
+
+		}
+		else if (strcmp(str, "print") == 0) {
+
+		}
+
 	}
-	Employee* employee = new Employee(name, age, country, job);
 
-	cout << employee;
+	
 	return 0;
-
 }
 
 void insert(char* name, int age, char* country, char* job) {
